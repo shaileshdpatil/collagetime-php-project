b229f4deb801cdc6e7417a0c758e5e02e7ce6d40
shaileshdpatil <69628770+shaileshdpatil@users.noreply.github.com>
2020-12-24 16:27:31 +0530

    Add files via upload

---
+++ elearning/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- font awesome file -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <title>collagetime</title>
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- start navigation -->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">CollageTime</a>
+    <span class="navbar-text">learn for earn</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav pl-5">
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Home</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Cources</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment status</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My profile</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li> 
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li> 
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- ended navigatio -->
+
+<!-- video background -->
+<div class="container-fluid remove-vid-marg">
+    <div class="vid-parent">
+        <video playsinline autoplay muted loop>
+        <source src="video/background.mp4"  >
+        </video>
+        <div class="vid-overlay"></div>
+    </div>
+<div class="vid-content">
+    <h1 class="my-content head-vid">welcome to collageTime</h1><br>
+    <small class="my-content">Learn for earn</small></br><br>   
+    <a href="a" class="btn btn-danger">get started</a>
+
+</div>
+</div>
+<!-- video ended -->
+
+<!-- text banner -->
+<!-- <div class="container-fluid bg-danger txt-banner">
+    <div class="row-bottom-banner">
+        <div class="col-sm">
+            <h5><i class="fas fa-book-open mr-3"></i>100+ cources</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fas fa-users mr-3"></i>expert peoples</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fas fa-keyboard mr-3"></i>lifetime access</h5>
+        </div>
+        
+        <div class="col-sm">
+            <h5><i class="fas fa-rupee-sign mr-3"></i>money back guarentee</h5>
+        </div>
+
+    </div>
+</div> -->
+<!-- end banner -->
+
+<!-- cources for student -->
+<h1 class="text-center" style="background-color: black; font-size:60px">Popular Languages</h1>
+<div class="container mt-5">
+ 
+  <div class="card-deck mt-4">
+  <a href="#" class="btn" style="text-align:left;padding:0px;margin:0px;">
+  <div class="card">
+  <img src="#" alt="card-img-top" alt="guitar" />
+  <div class="card-body">
+  <h5 class="card-title">Learn python in easyest way.</h5>
+  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cupiditate? Sunt commodi sequi qui distinctio excepturi blanditiis dignissimos ex itaque error. Aliquid fugit inventore porro esse voluptates repellendus ipsa quia!</p>
+  
+  </div>
+  <div class="card-footer">
+  <p class="card-text d-inline">Price :<small><del> 2000</del></small>
+  <span class="font-weight-bolder">&#8377 200<span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+    </div>
+  </div>
+</a>
+
+<div class="card-deck mt-4">
+  <a href="#" class="btn" style="text-align:left;padding:0px;margin:0px;">
+  <div class="card">
+  <img src="#" alt="card-img-top" alt="guitar" />
+  <div class="card-body">
+  <h5 class="card-title">Learn python in easyest way.</h5>
+  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cupiditate? Sunt commodi sequi qui distinctio excepturi blanditiis dignissimos ex itaque error. Aliquid fugit inventore porro esse voluptates repellendus ipsa quia!</p>
+  
+  </div>
+  <div class="card-footer">
+  <p class="card-text d-inline">Price :<small><del> 2000</del></small>
+  <span class="font-weight-bolder">&#8377 200<span></p>
+      <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+    </div>
+  </div>
+</a>
+
+
+
+
+<!-- second -->
+
+
+<div class="container mt-5">
+
+  <div class="card-deck mt-4">
+  <a href="#" class="btn" style="text-align:left;padding:0px;margin:0px;">
+  <div class="card">
+  <img src="#" alt="card-img-top" alt="laravel" />
+  <div class="card-body">
+  <h5 class="card-title" style="top-">Learn Laravel in easyest way.</h5>
+  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cupiditate? Sunt commodi sequi qui distinctio excepturi blanditiis dignissimos ex itaque error. Aliquid fugit inventore porro esse voluptates repellendus ipsa quia!</p>
+  
+  </div>
+  <div class="card-footer">
+  <p class="card-text d-inline">Price :<small><del> 2000</del></small><span class="font-weightbolder">&#8377 200</p>
+    <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+
+    
+    </div>
+  </div>
+
+</div>
+
+<!-- third section -->
+
+<div class="container mt-5">
+
+  <div class="card-deck mt-4">
+  <a href="#" class="btn" style="text-align:left;padding:0px;margin:0px;">
+  <div class="card">
+  <img src="#" alt="card-img-top" alt="angular" />
+  <div class="card-body">
+  <h5 class="card-title">Learn Angular easyest way.</h5>
+  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cupiditate? Sunt commodi sequi qui distinctio excepturi blanditiis dignissimos ex itaque error. Aliquid fugit inventore porro esse voluptates repellendus ipsa quia!</p>
+  
+  </div>
+  <div class="card-footer">
+  <p class="card-text d-inline">Price :<small><del> 2000</del></small><span class="font-weightbolder">&#8377 200</p>
+    <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+    </div>
+  </div>
+
+</div>
+
+<!-- ended section -->
+
+
+<!-- ended nevigation -->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- font awesome -->
+<script src="js/all.min.js"></script>
+</body>
+</html>
